72ad6a8e97adc04d9fd7abcfc069f93890e933a0
Ollie-Judge <ollie.judge@hotmail.com>
2022-12-14 14:44:35 +0000

    removed the number only restriction but still not working properly

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const getUserInput = () => {
   let weight = parseInt(document.getElementById("weightInput").value);
   let height = parseInt(document.getElementById("heightInput").value);
 
-  console.log(weight);
-  console.log(height);
+  console.log(Math.pow(height, 2));
 
-  let BMI = weight / (height * height);
+  let BMI = weight / Math.pow(height, 2);
   console.log(BMI);
 
   document.getElementById("bmiOutput").innerHTML = `${BMI}`;
